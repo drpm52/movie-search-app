4d44f93aa1f6eaa367e90e8ec28501a7628d1775
Parisa <drpm52@gmail.com>
2023-07-19 21:02:22 -0700

    add initial JavaScript code

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 "use strict";
+const inputEl = document.querySelector(".search-input-area");
+const searchBtn = document.querySelector(".search-btn");
 
 // Here is your key: 8149b202
 // https://www.omdbapi.com/
@@ -15,4 +17,25 @@
 
 // const rating = "8.8/10"
 // console.log(rating.slice(0,-3))
-
+async function searchByTitle() {
+  try {
+    const response = await fetch(
+      `http://www.omdbapi.com/?t=${inputEl.value}&apikey=8149b202`
+ 
+    );
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+console.log(typeof inputEl.value);
+searchByTitle("inception");
+searchBtn.addEventListener("click", 
+  (e) => {
+    e.preventDefault();
+    searchByTitle(inputEl.value);
+  }
+);
+// fetch(`http://www.omdbapi.com/?t=inception&apikey=8149b202`).then(resp=> resp.json().then(data=> console.log(data)))
