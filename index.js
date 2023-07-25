0d0bbf9bcc307414d0a491247d3122074583ed14
Parisa <drpm52@gmail.com>
2023-07-25 12:27:40 -0700

    final version

--- index.js
+++ index.js
@@ -3,12 +3,9 @@
 const inputEl = document.querySelector(".search-input-area");
 const searchBtn = document.getElementById("search-btn");
 const movieDivEl = document.querySelector(".movie-div");
-const myWatchListEl  = document.getElementById("watch-list")
-// const myWatchListEl = document.querySelector("#watch-list")
+const myWatchListEl = document.getElementById("watch-list");
 
 let searchResultArray = [];
-// let myWatchList = localStorage.getItem("watchListMovieIds")|| [];
-
 
 async function searchByTitle() {
   try {
@@ -18,7 +15,7 @@ async function searchByTitle() {
     const data1 = await response.json();
     let movieArray = data1.Search;
 
-    // console.log(movieArray);
+    console.log(movieArray);
 
     const moviePromises = movieArray.map(async (movie) => {
       const repsonse2 = await fetch(
@@ -28,17 +25,13 @@ async function searchByTitle() {
       return repsonse2.json();
     });
 
-    // searchResultArray = await Promise.all(moviePromises);
-    searchResultArray = await Promise.allSettled(moviePromises);
     searchResultArray = await Promise.all(moviePromises);
-
-    // console.log(searchResultArray);
-
+    console.log(searchResultArray);
     let movieHtml = searchResultArray
-    
+
       .map((movie) => renderHTML(movie))
       .join("");
-    // console.log(movieHtml);
+
     movieDivEl.innerHTML = movieHtml;
   } catch (err) {
     movieDivEl.innerHTML = `<div class="error-msg" > Unable to find what you're looking for. Please try another search.</div>`;
@@ -63,7 +56,7 @@ function renderHTML(movie) {
     </div>
 
     <div class = "movie-info-text row gap-one" >
-       <p class="runtime">${movie.runTime}</p>
+       <p class="runtime">${movie.Runtime}</p>
        <p class="genre">${movie.Genre}</p>
        <div class="add-div">
        <img class ="add" src="images/plus-no-bg.png" alt="add button with plus sign">
@@ -80,116 +73,24 @@ function renderHTML(movie) {
  `;
   return html;
 }
-// function renderWatchlistHtml(data) {
-//   let html = `
-//   <div class = "movie-card row gap-one center">
-//   <div class="movie-poster row center ">
-//       <img class ="movie-poster-img" src=${data.poster} alt="poster for ${
-//     data.title
-//   }"/>
-//   </div>
 
-//   <div class = "movie-info-div row col  ">
-//      <div class="movie-title-rating-div row  center ">
-//         <h3 class ="movie-title">${data.title}</h3>
-//         <img class= "star" src= "./images/star.png" alt="star emoji">
-//         <p class= "rating">${data.rating}</p>
-//     </div>
+document.querySelector(".movie-div").addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("add-btn")) {
+    const id = e.target.id;
 
-//     <div class = "movie-info-text row gap-one" >
-//        <p class="runtime">${data.runTime}</p>
-//        <p class="genre">${data.genre}</p>
+    const savedMovieEl = e.target.closest(".movie-card");
 
-//        <div class="remove-div">
-//        <img class ="remove" src="images/minus-icon-no-bg.png" alt="remove button with minus sign">
-//         <button class ="remove-btn" >Remove</button>
-//        </div>
-//       </div>
-//       <div class = "movie-plot-div">
-//       <p class="movie-plot">${data.moviePlot}</p>
+    const watchListMovie = {
+      id: e.target.id,
+      poster: savedMovieEl.querySelector(".movie-poster-img").src,
+      title: savedMovieEl.querySelector(".movie-title").innerText,
+      rating: savedMovieEl.querySelector(".rating").innerText,
+      runTime: savedMovieEl.querySelector(".runtime").innerText,
+      genre: savedMovieEl.querySelector(".genre").innerText,
+      moviePlot: savedMovieEl.querySelector(".movie-plot").innerText,
+    };
 
-//     </div>
-//     </div>
-//   </div>
-  
-//  `;
-//   return html;
-// }
-
-
-  document.querySelector(".movie-div").addEventListener("click", function (e) {
-    e.preventDefault()
-    if (e.target.classList.contains("add-btn")) {
-    
-  
-  
-  const id = e.target.id 
-  
-  const savedMovieEl= e.target.closest('.movie-card')
- 
-  const watchListMovie = {
-    id: e.target.id,
-    poster: savedMovieEl.querySelector('.movie-poster-img').src,
-    title:savedMovieEl.querySelector('.movie-title').innerText,
-    rating: savedMovieEl.querySelector('.rating').innerText,
-    runTime: savedMovieEl.querySelector('.runtime').innerText,
-    genre: savedMovieEl.querySelector('.genre').innerText,
-    moviePlot:savedMovieEl.querySelector(".movie-plot").innerText
-  }
-
-  
-      localStorage.setItem(id ,JSON.stringify(watchListMovie))
-      let data = JSON.parse(localStorage.getItem(id.toString()));
-       let watchListMovieHtml =renderWatchlistHtml(data)
-       console.log(watchListMovieHtml);
-       myWatchListEl.innerHTML =`<$ class = "movie-card row gap-one center">${watchListMovieHtml} </div>`
-    }
-  });
-   
-
-
-
-// localStorage.clear()
-
-// let watchlist =JSON.parse(localStorage.getItem('data')) || [];
-
-
-  //  localStorage.setItem("data", JSON.stringify( movieId))
-  //  console.log(localStorage.getItem('data', JSON.parse(movieId)))
-
-  // searchResultArray.find()
-
-  // let removeDiv = document.createElement('div');
-  // removeDiv.classList.add('remove-div')
-  // removeDiv.innerHTML = `<img class ="remove" src="images/minus-icon-no-bg.png" alt="remove button with minus sign">
-  //   <button class ="remove-btn" >Remove</button>`
-  //  let addDiv = movieDivEl.querySelector('.add-div')
-
-  //  addDiv.replaceWith(removeDiv)
-  //  let data = JSON.parse(movieDivEl.innerHTML);
-  //   watchlist.push(data)
-
-  //   localStorage.setItem('data',JSON.stringify(watchlist))
-
-  
-  //    let removeDiv = document.createElement('div');
-  // removeDiv.classList.add('remove-div')
-  // removeDiv.innerHTML = `<img class ="remove" src="images/minus-icon-no-bg.png" alt="remove button with minus sign">
-  //   <button class ="remove-btn" >Remove</button>`
-  //  let addDiv = movieDivEl.querySelector('.add-div')
- 
-  //  addDiv.replaceWith(removeDiv)
-  //  const savedMovieHtml= e.target.closest('.movie-card').innerHTML 
-  //  let addDiv = movieDivEl.querySelector('.add-div')
-
-   // const poster = savedMovieEl.querySelector('.movie-poster-img').innerText
-  // const title =savedMovieEl.querySelector('.movie-title').innerText
-  // const rating = savedMovieEl.querySelector('.rating').innerText
-  // const runTime = savedMovieEl.querySelector('.runtime').innerText
-  // const genre = savedMovieEl.querySelector('.genre').innerText
-  // const moviePlot = savedMovieEl.querySelector('.movie-plot').innerText
-
-  //  addDiv.replaceWith(removeDiv)
-    //  console.log(id, watchListMovie);
-
-       // myWatchListEl.innerHTML = addMovieToWatchlist(e);
+    localStorage.setItem(id, JSON.stringify(watchListMovie));
+  } else return;
+});
--- watchListIndex.js
+++ watchListIndex.js
@@ -1,32 +1,21 @@
 "use strict";
 
+const myWatchListEl = document.getElementById("watch-list");
 
+let watchListMovies = Object.values(localStorage);
 
- const myWatchListEl  = document.getElementById("watch-list")
- 
-let watchListMovies = Object.values(localStorage)
-// console.log(watchListMovies);
-    let watchListMoviesArray = watchListMovies.map(el => JSON.parse(el))
-   
-    console.log(watchListMoviesArray);
- let watchListHtml = ""
-watchListHtml += watchListMoviesArray.map(data => renderWatchlistHtml(data) )
+let watchListMoviesArray = watchListMovies.map((el) => JSON.parse(el));
+
+let watchListHtml = "";
+watchListHtml += watchListMoviesArray.map((data) => renderWatchlistHtml(data));
 //  console.log(watchListHtml);
-   myWatchListEl.innerHTML = watchListHtml;
-   
-// console.log(watchListMovieObjects);
-// console.log( watchListMovieObjects);
-// const movieData = watchListMovieObjects
-// console.log(typeof movieData);
+myWatchListEl.innerHTML = watchListHtml;
 
-
- function renderWatchlistHtml(data) {
-    let html = `
+function renderWatchlistHtml(data) {
+  let html = `
     <div class = "movie-card row gap-one center">
     <div class="movie-poster row center ">
-        <img class ="movie-poster-img" src=${data.poster} alt="poster for ${
-      data.title
-    }"/>
+        <img class ="movie-poster-img" src=${data.poster} alt="poster for ${data.title}"/>
     </div>
   
     <div class = "movie-info-div row col">
@@ -42,7 +31,7 @@ watchListHtml += watchListMoviesArray.map(data => renderWatchlistHtml(data) )
   
          <div class="remove-div">
          <img class ="remove" src="images/minus-icon-no-bg.png" alt="remove button with minus sign">
-          <button class ="remove-btn" >Remove</button>
+          <button class ="remove-btn"  id = "${data.id}" >Remove</button>
          </div>
         </div>
         <div class = "movie-plot-div">
@@ -53,100 +42,17 @@ watchListHtml += watchListMoviesArray.map(data => renderWatchlistHtml(data) )
     </div>
     
    `;
-    return html;
+  return html;
+}
+
+myWatchListEl.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("remove-btn")) {
+    removeMovieFromWatchList(e);
   }
-  
-//  console.log(myWatchListEl);
+});
 
-// let watchListMovieIds =localStorage.getItem('data')
-// // watchListMovieIds = [watchListMovieIds]
-// console.log( watchListMovieIds);
-// // myWatchListEl.innerHTML += watchListMovieIds.map(id => {
-    
-// //  return addMovieToWatchlist(id)
-    
-// // });
-
-
-
-
-// function renderWatchlistHtml(data) {
-//     let html = `
-//     <div class = "movie-card row gap-one center">
-//     <div class="movie-poster row center ">
-//         <img class ="movie-poster-img" src=${data.Poster} alt="poster for ${
-//       data.Title
-//     }"/>
-//     </div>
-  
-//     <div class = "movie-info-div row col  ">
-//        <div class="movie-title-rating-div row  center ">
-//           <h3 class ="movie-title">${data.Title}</h3>
-//           <img class= "star" src= "./images/star.png" alt="star emoji">
-//           <p class= "rating">${data.Ratings[0].Value.slice(0, -3)}</p>
-//       </div>
-  
-//       <div class = "movie-info-text row gap-one" >
-//          <p class="runtime">${data.Runtime}</p>
-//          <p class="genre">${data.Genre}</p>
-  
-//          <div class="remove-div">
-//          <img class ="remove" src="images/minus-icon-no-bg.png" alt="remove button with minus sign">
-//           <button class ="remove-btn" >Remove</button>
-//          </div>
-//         </div>
-//         <div class = "movie-plot-div">
-//         <p class="movie-plot">${data.Plot}</p>
-  
-//       </div>
-//       </div>
-//     </div>
-    
-//    `;
-//     return html;
-//   }
-//   async function addMovieToWatchlist(id) {
-//     // http://www.omdbapi.com/?i=tt5697572&apikey=8149b202
-  
-//     try {
-//       const promise = await fetch(
-//         `http://www.omdbapi.com/?i=${id}&apikey=8149b202`
-//       );
-//       const data = await promise.json();
-//     //  console.log(data);
-    
-     
-//      let watchListMovies = ""
-//       watchListMovies =renderWatchlistHtml(data);
-//       // myWatchListEl.innerHTML = watchListMovie
-//       console.log(watchListMovies);
-//       return watchListMovies;
-  
-//     } catch (error) {
-//     //   console.log(error);
-//     }}
-
-// console.log(addMovieToWatchlist("tt5697572"));
-
-// // function removeMovieFromWatchlist(movieId){
-// // let watchlist =JSON.parse(localStorage.getItem('data')) || []
-// // let indexToRemove = watchlist.findIndex(el => el.id === movieId);
-// // console.log(indexToRemove);
-// // }
-//  // const myWatchlist= document.querySelector('.my-watchlist')
-// // const data =localStorage.getItem("data")
-
-// // myWatchlist.innerHTML += localStorage.getItem("data")
-
-
-// // localStorage.clear()
-// // document.querySelector('.remove-btn').addEventListener('click', function(e){
-// //     let movieId = e.target.closest('.movie-card').id
-// //     localStorage.getItem("data.movieId ")
-
-// //     removeMovieFromWatchlist(movieId)
-
-
-// //  })
-// //  console.log(movieIndex);
-
+function removeMovieFromWatchList(e) {
+  e.target.closest(".movie-card").remove();
+  localStorage.removeItem(e.target.id);
+}
